d765b0aa6892e62f344369ea1cf07d6e251dc660
yaooooooooooooooo <62118705+yaooooooooooooooo@users.noreply.github.com>
2022-04-20 13:45:37 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x3F03441C347ED025f5A03933fC8753BfEf75F02e",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
     "title": "My NFT Collection",
